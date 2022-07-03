a3e2a68cd8c69feb1c1d4c37b93f815377f5ecd8
YounByoungil <jimmy7525@naver.com>
2022-07-04 01:43:57 +0900

    Setup Project

---
+++ NRCOnboarding/NRCOnboarding/OnboardingViewController.swift
@@ -0,0 +1,27 @@
+//
+//  OnboardingViewController.swift
+//  NRCOnboarding
+//
+//  Created by 윤병일 on 2022/07/04.
+//
+
+import UIKit
+
+class OnboardingViewController : UIViewController {
+  
+  //MARK: - Properties
+  
+  //MARK: - Lifecycle
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    configureUI()
+  }
+  
+  //MARK: - Functions
+  private func configureUI() {
+    view.backgroundColor = .red
+  }
+  
+  //MARK: - @objc func
+  
+}
