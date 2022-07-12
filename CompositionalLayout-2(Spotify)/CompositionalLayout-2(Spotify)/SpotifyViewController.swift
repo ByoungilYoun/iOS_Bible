1dfc1d10cbedd710628b2670d8610dde7c85ab52
YounByoungil <jimmy7525@naver.com>
2022-07-13 01:39:17 +0900

    [feat] Project Basic Setting

---
+++ CompositionalLayout-2(Spotify)/CompositionalLayout-2(Spotify)/SpotifyViewController.swift
@@ -0,0 +1,28 @@
+//
+//  SpotifyViewController.swift
+//  CompositionalLayout-2(Spotify)
+//
+//  Created by 윤병일 on 2022/07/13.
+//
+
+import UIKit
+import SnapKit
+
+class SpotifyViewController : UIViewController {
+  
+  //MARK: - Properties
+  
+  //MARK: - Lifecycle
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    configureUI()
+  }
+  
+  //MARK: - Functions
+  private func configureUI() {
+    view.backgroundColor = .red
+  }
+  
+  //MARK: - @objc func
+  
+}
