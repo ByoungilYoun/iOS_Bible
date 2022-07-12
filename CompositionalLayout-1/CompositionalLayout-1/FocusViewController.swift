3d1fdf8c7be061564c835f9fb59a4468947306da
YounByoungil <jimmy7525@naver.com>
2022-07-13 00:45:52 +0900

    [feat] 추천 버튼 기능 구현

--- CompositionalLayout-1/CompositionalLayout-1/FocusViewController.swift
+++ CompositionalLayout-1/CompositionalLayout-1/FocusViewController.swift
@@ -15,6 +15,18 @@ class FocusViewController : UIViewController {
     return UICollectionView(frame: .zero, collectionViewLayout: layout())
   }()
   
+  lazy var recommandButton : UIButton = {
+    let button = UIButton()
+    button.titleLabel?.font = UIFont.systemFont(ofSize: 16, weight: .bold)
+    button.setTitleColor(.white, for: .normal)
+    button.layer.cornerRadius = 10
+    button.backgroundColor = .systemTeal
+    button.addTarget(self, action: #selector(recommandButtonTapped), for: .touchUpInside)
+    return button
+  }()
+  
+  var showingRecommanded : Bool = false
+  
   var items : [Focus] = Focus.list
   
   typealias Item = Focus
@@ -30,18 +42,29 @@ class FocusViewController : UIViewController {
     super.viewDidLoad()
     configureUI()
     configureCollectionView()
+    updateButtonTitle()
   }
   
   //MARK: - Functions
   private func configureUI() {
     view.backgroundColor = .black
     
-    view.addSubview(collectionView)
+    [collectionView, recommandButton].forEach {
+      view.addSubview($0)
+    }
+    
     collectionView.backgroundColor = .black
     
     collectionView.snp.makeConstraints {
       $0.top.equalTo(view.safeAreaLayoutGuide)
       $0.leading.trailing.bottom.equalToSuperview()
+    }
+    
+    recommandButton.snp.makeConstraints {
+      $0.bottom.equalToSuperview().offset(-50)
+      $0.leading.equalToSuperview().offset(40)
+      $0.trailing.equalToSuperview().offset(-40)
+      $0.height.equalTo(50)
     }
   }
   
@@ -83,5 +106,24 @@ class FocusViewController : UIViewController {
     
     let layout = UICollectionViewCompositionalLayout(section: section)
     return layout
+  }
+  
+  private func updateButtonTitle() {
+    let title = showingRecommanded ? "See All" : "See Recommandation"
+    recommandButton.setTitle(title, for: .normal)
+  }
+  
+  //MARK: - @objc func
+  @objc private func recommandButtonTapped() {
+    showingRecommanded.toggle()
+    
+    self.items = showingRecommanded ? Focus.recommendations : Focus.list
+    
+    var snapShot = NSDiffableDataSourceSnapshot<Section, Item>()
+    snapShot.appendSections([.main])
+    snapShot.appendItems(items, toSection: .main)
+    dataSource.apply(snapShot)
+    
+    updateButtonTitle()
   }
 }
